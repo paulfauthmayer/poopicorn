ec6768cfc0011d4dbfacc39b66834f433a677c41
Paul Fauth-Mayer <paul@outlandish.com>
2017-06-24 19:55:52 +0100

    added url tracking, posts not being sent twice

--- extension/poopycorn/content_scripts/poopyOnSite.js
+++ extension/poopycorn/content_scripts/poopyOnSite.js
@@ -7,28 +7,52 @@ function getPosts(request, sender, sendResponse)
   let listOfPosts = document.getElementsByClassName('fbUserContent');
   for (let i = 0; i < listOfPosts.length;i++)
   {
+
+    if(isAlreadySend(listOfPosts[i]))
+    {
+      continue;
+    }
+
     let postData = getRelevantData(listOfPosts[i]);
-    if (postData !=  null && isNotSendYet(postData) )
+    if (postData)
     {
       sendPostData(postData);
     }
   }
 }
-function isNotSendYet(postData) {
-  if(true) {
+
+function isAlreadySend(post)
+{
+  if (post.id)
+  {
     return true;
-  } else {
+  }
+  else
+  {
     return false;
   }
 }
+
 function getRelevantData (fbUserContent)
 {
-  let titleEl = fbUserContent.getElementsByClassName("mbs")[0] ? fbUserContent.getElementsByClassName("mbs")[0].innerText : null;
-  let subtitleEl = fbUserContent.getElementsByClassName("_6m7")[0] ? fbUserContent.getElementsByClassName("_6m7")[0].innerText : null;
-  let descriptionEl = fbUserContent.getElementsByClassName("userContent")[0].getElementsByTagName("p")[0] ? fbUserContent.getElementsByClassName("userContent")[0].getElementsByTagName("p")[0].innerText : null;
-  let posterEl = fbUserContent.getElementsByTagName("h5")[0] ? fbUserContent.getElementsByTagName("h5")[0].innerText : null;
-  let websiteEl = fbUserContent.getElementsByClassName("ellipsis")[0] ? fbUserContent.getElementsByClassName("ellipsis")[0].innerText.toLowerCase() : null;
-
+  let titleEl = fbUserContent.getElementsByClassName("mbs")[0]
+              ? fbUserContent.getElementsByClassName("mbs")[0].innerText
+              : null;
+  let subtitleEl = fbUserContent.getElementsByClassName("_6m7")[0]
+                 ? fbUserContent.getElementsByClassName("_6m7")[0].innerText
+                 : null;
+  let descriptionEl = fbUserContent.getElementsByClassName("userContent")[0].getElementsByTagName("p")[0]
+                    ? fbUserContent.getElementsByClassName("userContent")[0].getElementsByTagName("p")[0].innerText
+                    : null;
+  let posterEl = fbUserContent.getElementsByTagName("h5")[0]
+               ? fbUserContent.getElementsByTagName("h5")[0].innerText
+               : null;
+  let websiteEl = fbUserContent.getElementsByClassName("ellipsis")[0]
+                ? fbUserContent.getElementsByClassName("ellipsis")[0].innerText.toLowerCase()
+                : null;
+  let urlEl = fbUserContent.getElementsByClassName("_52c6")[0]
+            ? fbUserContent.getElementsByClassName("_52c6")[0].getAttribute("href")
+            : null
   // let imageEl = fbUserContent.getElementsByClassName("scaledImageFitWidth")[0]
   //     ? fbUserContent.getElementsByClassName("scaledImageFitWidth")[0]
   //     : null;
@@ -42,12 +66,27 @@ function getRelevantData (fbUserContent)
       description: descriptionEl,
       website: websiteEl,
       poster: posterEl,
-
+      url: urlEl
     };
+
+    data.id = convertToSlug("poopicorn" + " " + posterEl + " " + titleEl)
+
+    fbUserContent.id = data.id
+
     return data;
   }
   return;
 }
+
+function convertToSlug(Text)
+{
+    return Text
+        .toLowerCase()
+        .replace(/[^\w ]+/g,'')
+        .replace(/ +/g,'-')
+        ;
+}
+
 
 function sendPostData(postData)
 {
@@ -58,5 +97,8 @@ function sendPostData(postData)
 
    browser.runtime.sendMessage({"data": postData});
 }
+
+
+function visualizeResults()
 
 browser.runtime.onMessage.addListener(getPosts);
