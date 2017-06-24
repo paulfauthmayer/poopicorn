3f22e439b79298c33399a3583995ffd6c58e2ef2
Paul Fauth-Mayer <paul@outlandish.com>
2017-06-24 13:19:19 +0100

    added scraper

--- extension/poopycorn/content_scripts/poopyOnSite.js
+++ extension/poopycorn/content_scripts/poopyOnSite.js
@@ -13,6 +13,20 @@ function getPosts(request, sender, sendResponse) {
 }
 
 
+function getRelevantData (fbUserContent) {
+  let data = {
+    title: fbUserContent.getElementsByClassName("mbs")[0] ? fbUserContent.getElementsByClassName("mbs")[0].innerText : null,
+    subtitle: fbUserContent.getElementsByClassName("_6m7")[0] ? fbUserContent.getElementsByClassName("_6m7")[0].innerText : null,
+    description: fbUserContent.getElementsByClassName("userContent")[0] ? fbUserContent.getElementsByClassName("userContent")[0].getElementsByTagName("p")[0].innerText : null,
+    image: fbUserContent.getElementsByClassName("scaledImageFitWidth")[0] ? fbUserContent.getElementsByClassName("scaledImageFitWidth")[0] : null,
+    poster: fbUserContent.getElementsByTagName("h5")[0] ? fbUserContent.getElementsByTagName("h5")[0].textContent : null,
+    website: fbUserContent.getElementsByClassName("ellipsis")[0] ? fbUserContent.getElementsByClassName("ellipsis")[0].innerText.toLowerCase() : null
+  }
+  return data
+}
+
+
+
 /*
 Assign beastify() as a listener for messages from the extension.
 */
